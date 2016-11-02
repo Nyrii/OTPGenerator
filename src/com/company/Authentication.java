071217492d3f09026714e74cb12ed82b7e575181
wilmot_g <guillaume.wilmot@epitech.eu>
2016-11-02 12:44:04 +0100

    add Test and lib

--- src/com/company/Authentication.java
+++ src/com/company/Authentication.java
@@ -64,46 +64,17 @@ public class Authentication {
 
 		long value = new Date().getTime() / TimeUnit.SECONDS.toMillis(30);
 
-//		byte[] data = new byte[8];
-//		for (int i = 8; i-- > 0; value >>>= 8) {
-//			data[i] = (byte) value;
-//		}
-
-		System.out.println("Time remaining : " + new Date().getTime() / 1000 % 30);
-
-//		byte[] hash = hmacSha1(data, key);
-//        return truncateHash(hash);
-		return String.valueOf(verifyCode(key, value));
-    }
-
-	int verifyCode(byte[] key, long t) throws Exception {
 		byte[] data = new byte[8];
-		long value = t;
 		for (int i = 8; i-- > 0; value >>>= 8) {
 			data[i] = (byte) value;
 		}
 
-		SecretKeySpec signKey = new SecretKeySpec(key, "HmacSHA1");
-		Mac mac = Mac.getInstance("HmacSHA1");
-		mac.init(signKey);
-		byte[] hash = mac.doFinal(data);
+		System.out.println("Time remaining : " + new Date().getTime() / 1000 % 30);
 
-		int offset = hash[20 - 1] & 0xF;
+		byte[] hash = hmacSha1(data, key);
+        return truncateHash(hash);
+    }
 
-		// We're using a long because Java hasn't got unsigned int.
-		long truncatedHash = 0;
-		for (int i = 0; i < 4; ++i) {
-			truncatedHash <<= 8;
-			// We are dealing with signed bytes:
-			// we just keep the first byte.
-			truncatedHash |= (hash[offset + i] & 0xFF);
-		}
-
-		truncatedHash &= 0x7FFFFFFF;
-		truncatedHash %= 1000000;
-
-		return (int) truncatedHash;
-	}
 
 	public static void main(String[] args) {
 		Authentication auth = new Authentication();
