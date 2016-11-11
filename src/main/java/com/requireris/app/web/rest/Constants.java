555020d77532505b1002a3f853171f52c29b3886
Nyrandone Noboud-Inpeng <noboud_n@epitech.eu>
2016-11-11 15:42:46 +0100

    Clean of the project.

---
+++ src/main/java/com/requireris/app/web/rest/Constants.java
@@ -0,0 +1,9 @@
+package com.requireris.app.web.rest;
+
+/**
+ * Created by noboud_n on 11/11/2016.
+ */
+public class Constants {
+    public final static String clientId = "772207505570-p9bst9hlg3tld1n3hfi9g6pns9g92gdl.apps.googleusercontent.com";
+    public final static String clientSecret = "wcbr_H63EWWwf-iDpssXCQUm";
+}
