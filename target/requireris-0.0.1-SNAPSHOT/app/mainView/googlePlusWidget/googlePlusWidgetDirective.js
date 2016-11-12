f0f181399005332908301a093cc40c86281e3dad
wilmot_g <guillaume.wilmot@epitech.eu>
2016-11-12 23:02:28 +0100

    Ready for presentation

---
+++ target/requireris-0.0.1-SNAPSHOT/app/mainView/googlePlusWidget/googlePlusWidgetDirective.js
@@ -0,0 +1,21 @@
+/**
+ * Created by wilmot_g on 07/11/16.
+ */
+
+'use strict';
+angular.module('requirerisApp')
+
+		.directive('googlePlusWidget', function() {
+
+			return {
+				restrict: 'AE',
+				templateUrl: "app/mainView/googlePlusWidget/googlePlusWidget.html",
+				controller: "googlePlusWidgetController",
+				scope: {
+					state: "@",
+					authenticated: "=",
+					csrf: '&'
+				}
+
+			}
+		});
